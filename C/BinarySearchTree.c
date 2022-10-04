88c93267e1a45272731995e7e6ae376e92a67265
thomunro <thomunro19@gmail.com>
2022-10-03 22:51:18 -0600

    2D game to C

--- C/BinarySearchTree.c
+++ C/BinarySearchTree.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
 
+typedef struct Node{
+    int data, left, right;
+} NODE;
+
+typedef struct binarySearchTree{
+    NODE root;
+} binarySearchTree;
+
+int factorial(int num){
+    int fact = 1;
+    if(num == 0) {   
+            return 1;    
+    }else {    
+        while(num > 1) {    
+            fact = fact * num;    
+            num--;    
+        }    
+        return fact;    
+    }
+}
+
+int numBinarySearchTrees(int key) {    
+        int catalanNumber = factorial(2 * key)/(factorial(key + 1) * factorial(key));    
+        return catalanNumber;    
+    }
+
 int main() {
+    binarySearchTree bt;
+    bt.root.data = 5;
+
+    printf("Number of possible Binary Search Trees with given key: %d", numBinarySearchTrees(bt.root.data));
+
     return 0;
 }
